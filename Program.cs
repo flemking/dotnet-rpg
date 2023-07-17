e11924888e7ada3e42901811822075e63ecdee26
Jean-Baptiste Babatounde Bohiki <xivmilbillions@gmail.com>
2023-07-17 04:04:30 +0100

    Added EF + Connection to SQL Server DB

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@ global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
