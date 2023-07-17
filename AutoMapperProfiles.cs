eb252047edd16b65f1a0fcb82540ea13d8bc471e
Jean-Baptiste Babatounde Bohiki <xivmilbillions@gmail.com>
2023-07-17 03:36:13 +0100

    Added all the CRUD operations

---
+++ AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Character, GetCharacterDto>();
+            CreateMap<AddCharacterDto, Character>();
+        }
+    }
+}
